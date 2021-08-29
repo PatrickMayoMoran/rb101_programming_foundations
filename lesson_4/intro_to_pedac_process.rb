870c828b703f2a5f2aa8984062081ec006f36e5f
Mayo <patrick.mayo.moran@gmail.com>
2021-08-29 14:54:55 -0400

    include example of problem solved with PEDAC process

--- lesson_4/intro_to_pedac_process.rb
+++ lesson_4/intro_to_pedac_process.rb
@@ -17,13 +17,51 @@
 # Rules: Explicit
 # -Case matters: 'Mom' is not a palindrome, 'mom' is
 # -Must return all substrings, not just the first one encountered
-#
-#
-#
 
-# Rules: Implicit
-#
-#
-#
-#
-#
+# Rules: Implicit gleaned from test cases
+# -return data structure is an array, even if one/no strings are palindromes
+# -palindromes stored in order they are encountered from starting letter onward
+# -empty string input should be returned as empty array
+# -single letters are not palindromes, must be at least 2 letters
+
+# Pseudocode
+# create empty array
+# fill array with substrings that are two chars or greater in length
+# create empty array
+# fill array with substrings that are palindromes
+# return array of palindromes
+
+# get substrings of 2 or greater from a string
+# start at beginning of string (index 0) and go up to
+# get every substring from length 2 to length of string
+# go to next char in string (index +1)
+
+def get_substrings(string)
+  substrings_array = []
+  index = 0
+  while index <= string.length - 2
+    size = 2
+    while size <= string.length - index
+      substrings_array << string.slice(index, size)
+      size += 1
+    end
+    index += 1
+  end
+end
+
+def is_palindrome?(string)
+  string == string.reverse
+end
+
+def palindrome_substrings(string)
+  substrings_array = []
+  substrings_array = get_substrings(string)
+  palindrome_substrings = []
+  substrings_array.each do |string|
+    palindrome_substrings << string if is_palindrome?(string)
+  end
+  return palindrome_substrings
+
+  palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
+  palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
+  palindrome_substrings("palindrome") == []
