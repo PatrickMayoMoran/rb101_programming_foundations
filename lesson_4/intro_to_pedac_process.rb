7ec959df0316136ac4b474316eaf013caf1d4c49
Mayo <patrick.mayo.moran@gmail.com>
2021-08-28 21:01:25 -0400

    begin lesson_4 intro to pedac

---
+++ lesson_4/intro_to_pedac_process.rb
@@ -0,0 +1,29 @@
+# PROBLEM:
+
+# Given a string, write a method `palindrome_substrings` which returns
+# all the substrings from a given string which are palindromes. Consider
+# palindrome words case sensitive.
+
+# Test cases:
+
+# palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
+# palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
+# palindrome_substrings("palindrome") == []
+# palindrome_substrings("") == []
+
+# Input: string
+# Output: array of strings
+
+# Rules: Explicit
+# -Case matters: 'Mom' is not a palindrome, 'mom' is
+# -Must return all substrings, not just the first one encountered
+#
+#
+#
+
+# Rules: Implicit
+#
+#
+#
+#
+#
