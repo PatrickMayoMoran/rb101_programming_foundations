7b5b29b393d40c3b72fa1f243ae971a86b180610
Mayo <patrick.mayo.moran@gmail.com>
2021-09-01 03:42:15 -0400

    include array multiplication transformation implementation

--- lesson_4/selection_and_transformation.rb
+++ lesson_4/selection_and_transformation.rb
@@ -7,7 +7,7 @@ def select_fruit(dict)
     break if counter == keys.size # in case the hash is empty
     
     current_key = keys[counter]
-    current_value = dicts[current_key]
+    current_value = dict[current_key]
 
     if current_value == "Fruit"
       fruits_dict[current_key] = "Fruit"
@@ -27,3 +27,22 @@ produce = {
 }
 
 select_fruit(produce) # => {"apple"=>"Fruit", "pear"=>"Fruit"}
+
+def multiply(array, factor)
+  multiplied_numbers = []
+  counter = 0
+
+  loop do
+    break if array.size == counter
+
+    current_value = array[counter]
+    multiplied_numbers[counter] = current_value * factor
+
+    counter += 1
+  end
+
+  multiplied_numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+multiply(my_numbers, 3) # => [3, 12, 9, 21, 6, 18]
