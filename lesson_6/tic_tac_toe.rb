8fcfe54b1131496e15ece58da2c7925be27ca805
Mayo <patrick.mayo.moran@gmail.com>
2021-10-03 18:07:33 -0400

    include improved join function and game to #

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -11,6 +12,14 @@ end
 
 def empty_squares(brd)
   brd.keys.select { |square| brd[square] == INITIAL_MARK }
+end
+
+def joinor(array, joiner=', ', word=' or ')
+  return array.first if array.length <= 1
+  return array.join(word) if array.length == 2
+  if array.length > 2
+    array[0...-1].join(joiner) + word + array.last.to_s
+  end
 end
 
 # rubocop:disable Metrics/AbcSize
@@ -42,7 +51,7 @@ end
 def player_marks_spot!(board)
   square = ''
   loop do
-    prompt "Choose a square: #{empty_squares(board).join(', ')}"
+    prompt "Choose a square: #{joinor(empty_squares(board))}"
     square = gets.chomp.to_i
     break if empty_squares(board).include?(square)
     prompt "Not a valid choice, choose again"
@@ -74,23 +83,56 @@ def detect_winner(board)
   nil
 end
 
+def report_winner(player_wins, computer_wins)
+  if player_wins == 5
+    prompt "You won!"
+    true
+  elsif computer_wins == 5
+    prompt "The Computer won!"
+    true
+  else
+    false
+  end
+end
+
 loop do
-  board = initialize_board
+  player_wins = 0
+  computer_wins = 0
 
   loop do
+    board = initialize_board
+
+    loop do
+      display_board(board)
+      player_marks_spot!(board)
+      break if winner?(board) || board_full?(board)
+      computer_marks_spot!(board)
+      break if winner?(board) || board_full?(board)
+    end
+
     display_board(board)
-    player_marks_spot!(board)
-    break if winner?(board) || board_full?(board)
-    computer_marks_spot!(board)
-    break if winner?(board) || board_full?(board)
-  end
 
-  display_board(board)
+    if winner?(board)
+      prompt "#{detect_winner(board)} won!"
+    else
+      prompt "It's a tie!"
+    end
 
-  if winner?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
+    case detect_winner(board)
+    when 'Player' then player_wins += 1
+    when 'Computer' then computer_wins += 1
+    end
+
+    prompt "You have won #{player_wins} and the computer has won #{computer_wins}."
+    
+    break if report_winner(player_wins, computer_wins)
+    
+    loop do
+      prompt "Ready for the next one? y/n"
+      choice = gets.chomp.downcase
+      break if choice.start_with?('y')
+      prompt "Take your time... the computer will wait"
+    end
   end
 
   prompt "Would you like to play again? y/n"
